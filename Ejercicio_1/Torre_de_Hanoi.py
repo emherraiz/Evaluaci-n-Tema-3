15290c9d844fcdbf8d7ba083591814756995f3ac
Enrique Marina <eherraiz@myuax.com>
2022-10-29 17:04:32 +0200

    seguimoa

--- Ejercicio_1/Torre_de_Hanoi.py
+++ Ejercicio_1/Torre_de_Hanoi.py
@@ -1,10 +1,28 @@
-def Torre_de_Hanoi(n, aguja_inicial, aguja_final, aguja_intermedia):
-    if n==1:
-        print ("Movemos el disco 1 desde",aguja_inicial,"hasta",aguja_final)
-        return
-    Torre_de_Hanoi(n-1, aguja_inicial, aguja_intermedia, aguja_final)
-    print ("Movemos el disco",n,"desde",aguja_inicial,"a",aguja_final)
-    Torre_de_Hanoi(n-1, aguja_intermedia, aguja_final, aguja_inicial)
-# Driver code
-n = 64
-Torre_de_Hanoi(n,'A','B','C')
+def move_tower(height, from_pole, to_pole, with_pole):
+    """
+    >>> move_tower(3, 'A', 'B', 'C')
+    moving disk from A to B
+    moving disk from A to C
+    moving disk from B to C
+    moving disk from A to B
+    moving disk from C to A
+    moving disk from C to B
+    moving disk from A to B
+    """
+    if height >= 1:
+        move_tower(height - 1, from_pole, with_pole, to_pole)
+        move_disk(from_pole, to_pole)
+        move_tower(height - 1, with_pole, to_pole, from_pole)
+
+
+def move_disk(fp, tp):
+    print("moving disk from", fp, "to", tp)
+
+
+def main():
+    height = int(input("Height of hanoi: ").strip())
+    move_tower(height, "A", "B", "C")
+
+
+if __name__ == "__main__":
+    main()
--- Ejercicio_2/determinante.py
+++ Ejercicio_2/determinante.py
@@ -4,22 +4,15 @@ matrizx = [[1,1],[1,2]]
 
 
 def determinante(A):
-    if isinstance(A, int):
-        det = A
-        return det
+    if len(A) == 1:
+        return A[0]
 
-    det = 0
-    fila_1 = A[0][:]
-    A[0][:] = []
+    elif len(A) == 2:
+        return A[0][0]*A[1][1] - A[0][1] * A [1][0]
 
-    for i in range(len(A[0])):
-        A_i = A
-        A_i[:][i] = []
-
-        det += pow((-1), (i+1)) * fila_1[i] * determinante(A_i)
-
-
-A = [[2, 8, 3, 4],[2, 2, 3, 4],[2,3,4,1],[5,7,3,4]]
-print(isinstance(4, int))
+    else:
+        for i in range(0, len(A)):
+            total += ((-1)**i)*A[0][i]*determinante(A[i])
+A = [[1, 2, 3, 4],[1, 2, 3, 4],[2,3,4,1],[5,7,3,4]]
 print(pow((-1), (2+1)))
-print(determinante(A))
+#print(determinante(A))
